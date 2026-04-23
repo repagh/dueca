d27283a2db311c3d7c4cdfc3c4a37f3d44a5a06b
Rene van Paassen <rene.vanpaassen@gmail.com>
2026-04-23 15:41:39 +0200

    fix gtk3 buttons fedora

--- dueca/gui/gtk3/GtkDuecaButtons.cxx
+++ dueca/gui/gtk3/GtkDuecaButtons.cxx
@@ -11,16 +11,38 @@
         license         : EUPL-1.2
 */
 
-
 #define GtkDuecaButtons_cxx
 #include "GtkDuecaButtons.hxx"
 
 #include <DuecaPath.hxx>
+#include <debug.h>
 
+static GdkPixbuf *emergency_icons[2] = { NULL, NULL };
+static GdkPixbuf *button_icons[5] = { NULL, NULL, NULL };
 
-static void toggle_visibility(GtkWidget* widget, gpointer data)
+static GdkPixbuf *loadTextureFromFile(const char *fname)
 {
-  if ( (*((int*)data))-- == 0) {
+  GError *error = NULL;
+  auto tex =
+    gdk_pixbuf_new_from_file(dueca::DuecaPath::prepend(fname).c_str(), &error);
+  if (error) {
+    /* DUECA UI.
+
+       Cannot load icon texture. Check DUECA installation.
+    */
+    E_XTR("Could not load texture \"" << dueca::DuecaPath::prepend(fname)
+                                      << "\": " << error->message);
+    g_error_free(error);
+    error = NULL;
+    return NULL;
+  }
+  g_object_ref(G_OBJECT(tex));
+  return GDK_PIXBUF(tex);
+}
+
+static void toggle_visibility(GtkWidget *widget, gpointer data)
+{
+  if ((*((int *)data))-- == 0) {
     gtk_widget_show(widget);
   }
   else {
@@ -28,17 +50,26 @@ static void toggle_visibility(GtkWidget* widget, gpointer data)
   }
 }
 
-
-void gtk_dueca_button_set_image(GtkWidget* widget, gint imgno)
+void gtk_dueca_button_set_image(GtkWidget *widget, gint imgno)
 {
   g_return_if_fail(widget != NULL);
   g_return_if_fail(GTK_IS_CONTAINER(widget));
   gtk_container_foreach(GTK_CONTAINER(gtk_bin_get_child(GTK_BIN(widget))),
-                        toggle_visibility, (gpointer) &imgno);
+                        toggle_visibility, (gpointer)&imgno);
 }
 
-GtkWidget* NewGtkDuecaButton_pixmaps()
+GtkWidget *NewGtkDuecaButton_pixmaps()
 {
+  static bool loaded = false;
+  if (!loaded) {
+    button_icons[0] = loadTextureFromFile("pixmaps/inactive.png");
+    button_icons[1] = loadTextureFromFile("pixmaps/inprogress.png");
+    button_icons[2] = loadTextureFromFile("pixmaps/active.png");
+    button_icons[3] = loadTextureFromFile("pixmaps/incomplete.png");
+    button_icons[4] = loadTextureFromFile("pixmaps/fell-back.png");
+    loaded = true;
+  }
+
   //GdkBitmap *mask = NULL;
   //cairo_surface_t *pm;
   GtkWidget *pixmap;
@@ -48,29 +79,24 @@ GtkWidget* NewGtkDuecaButton_pixmaps()
   //pm = cairo_image_surface_create_from_xpm
   //  (DUECA_NS ::DuecaPath::prepend("pixmaps/inactive.xpm").c_str());
 
-  pixmap = gtk_image_new_from_file
-    (DUECA_NS ::DuecaPath::prepend("pixmaps/inactive.xpm").c_str());
+  pixmap = gtk_image_new_from_pixbuf(button_icons[0]);
   gtk_container_add(box, pixmap);
   gtk_widget_show(pixmap);
 
    // progress pixmap
-  pixmap = gtk_image_new_from_file
-    (DUECA_NS ::DuecaPath::prepend("pixmaps/inprogress.xpm").c_str());
+  pixmap = gtk_image_new_from_pixbuf(button_icons[1]);
   gtk_container_add(box, pixmap);
 
   // final pixmap
-  pixmap = gtk_image_new_from_file
-    (DUECA_NS ::DuecaPath::prepend("pixmaps/active.xpm").c_str());
+  pixmap = gtk_image_new_from_pixbuf(button_icons[2]);
   gtk_container_add(box, pixmap);
 
   // broken consistency pixmap
-  pixmap = gtk_image_new_from_file
-    (DUECA_NS ::DuecaPath::prepend("pixmaps/incomplete.xpm").c_str());
+  pixmap = gtk_image_new_from_pixbuf(button_icons[3]);
   gtk_container_add(box, pixmap);
 
   // fallback pixmap
-  pixmap = gtk_image_new_from_file
-    (DUECA_NS ::DuecaPath::prepend("pixmaps/fell-back.xpm").c_str());
+  pixmap = gtk_image_new_from_pixbuf(button_icons[4]);
   gtk_container_add(box, pixmap);
 
   // show the box
@@ -79,25 +105,30 @@ GtkWidget* NewGtkDuecaButton_pixmaps()
   return GTK_WIDGET(box);
 }
 
-GtkWidget* NewGtkDuecaAbortButton_pixmaps()
+GtkWidget *NewGtkDuecaAbortButton_pixmaps()
 {
+  // load the emergency and button icons
+  static bool loaded = false;
+  if (!loaded) {
+    emergency_icons[0] = loadTextureFromFile("pixmaps/abort.png");
+    emergency_icons[1] = loadTextureFromFile("pixmaps/confirm-abort.png");
+    loaded = true;
+  }
+
   // make a container with the many faces of this button
   GtkContainer *box = GTK_CONTAINER(gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0));
 
   // Abort pixmap
   GtkWidget *pixmap;
 
-  pixmap = gtk_image_new_from_file
-    (DUECA_NS ::DuecaPath::prepend("pixmaps/abort.xpm").c_str());
+  pixmap = gtk_image_new_from_pixbuf(emergency_icons[0]);
   gtk_container_add(box, pixmap);
   gtk_widget_show(pixmap);
 
    // Abort confirmation
-  pixmap = gtk_image_new_from_file
-    (DUECA_NS ::DuecaPath::prepend("pixmaps/confirm-abort.xpm").c_str());
+  pixmap = gtk_image_new_from_pixbuf(emergency_icons[1]);
   gtk_container_add(box, pixmap);
   gtk_widget_show(GTK_WIDGET(box));
 
   return GTK_WIDGET(box);
 }
-
