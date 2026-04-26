b1807f587efc1150da0bc45b774cdd41a566be17
René van Paassen <rene.vanpaassen@gmail.com>
2026-04-26 14:14:25 +0200

    [no ci] correct extra tests

--- test/extra/PolynomialNTest.cxx
+++ test/extra/PolynomialNTest.cxx
@@ -21,19 +21,27 @@ using namespace dueca;
 static double ai[] = { 1.0, 0.5, 1.0 };
 
 constexpr std::array<double, 4> aia{ 1.0, 0.5, 1.0 };
+template <typename FN>
+
+void check(const char* name, const FN& s, double u, double y)
+{
+  auto y2 = s(u);
+  std::cout << name << "(" << u << ") = " << y2 << " ? " << y << std::endl;
+  assert(std::abs(y-y2) < 1e-8);
+}
 
 int main()
 {
   PolynomialN s1(2, ai);
 
-  assert(s1(-1.0) == -0.5);
-  assert(s1(0.0) == 1.0);
-  assert(s1(1.0) == 2.5);
+  check("s1", s1, -1.0, 1.5);
+  check("s1", s1, 0.0, 1.0);
+  check("s1", s1, 1.0, 2.5);
 
   PolynomialN s2(aia);
-  assert(s1(-1.0) == -0.5);
-  assert(s1(0.0) == 1.0);
-  assert(s1(1.0) == 2.5);
+  check("s2", s2, -1.0, 1.5);
+  check("s2", s2, 0.0, 1.0);
+  check("s2", s2, 1.0, 2.5);
 
   return 0;
 }
